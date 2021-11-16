4a36cb2287d1e07579911122785dab01cfd40eca
Sherzodbek Ortikjonov <hhll69241@gmail.com>
2021-11-16 20:47:18 +0300

    Implement bubble sort

---
+++ sorting_algorithms/README.md
@@ -0,0 +1,6 @@
+## Sorting algorithms
+
+### Bubble sort
+A sorting algorithm where the largest values bubble up to the top.
+Bubble sort is probably good candidate when you are sorting nearly sorted array.
+Big O: best case is O(n) and the worst or average case is O(n^2)
---
+++ sorting_algorithms/bubble_sort.dart
@@ -0,0 +1,73 @@
+// Bubble sort
+
+void main() {
+  // print(bubbleSort([4, 5, 1, 0, -2]));
+  // print(bubbleSort([10, 2, 1, 2, 0, 1]));
+  // print(bubbleSort([]));
+  // print(bubbleSort([1]));
+
+  // print(bubbleSort2([4, 5, 1, 0, -2]));
+  // print(bubbleSort2([10, 2, 1, 2, 0, 1]));
+  // print(bubbleSort2([]));
+  // print(bubbleSort2([1]));
+
+  print(bubbleSort3([4, 5, 1, 0, -2]));
+  print(bubbleSort3([10, 2, 1, 2, 0, 1]));
+  print(bubbleSort3([]));
+  print(bubbleSort3([1]));
+}
+
+// Optimized
+List<int> bubbleSort3(List<int> arr) {
+  if (arr.length <= 1) return arr;
+
+  arr = arr.toList();
+  bool noSwap;
+  for (int i = arr.length; i > 0; i--) {
+    noSwap = true;
+    for (int j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwap = false;
+      }
+    }
+    if (noSwap) break;
+  }
+  return arr;
+}
+
+List<int> bubbleSort2(List<int> arr) {
+  if (arr.length <= 1) return arr;
+
+  arr = arr.toList();
+  for (int i = arr.length; i > 0; i--) {
+    for (int j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+List<int> bubbleSort(List<int> arr) {
+  if (arr.length <= 1) return arr;
+
+  arr = arr.toList();
+  int sortedNum = arr.length - 1;
+  for (int i = 0; i < arr.length; i++) {
+    for (int j = 0; j < sortedNum; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+    sortedNum--;
+  }
+  return arr;
+}
